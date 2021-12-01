9dd3d5f246aea75bf452e16aaefce736c23593df
Ola Rybacka <rybacka.ola@gmail.com>
2021-12-01 23:48:12 +0100

    add simple graphql fetch

--- frontend/src/components/cards/CardsContainer.tsx
+++ frontend/src/components/cards/CardsContainer.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useStore } from '../../store/useStore';
 import { CardItem } from './CardItem';
 import { Container, Slider, PrevButton, NextButton } from './CardStyles.styled';
+
+const headers = { "Content-Type": "application/json" };
 
 export const CardsContainer = () => {
   const { cards } = useStore();
@@ -13,6 +15,18 @@ export const CardsContainer = () => {
   const getPrevCard = () => {
     setIndex(index === 0 ? cards.length - 1 : index - 1);
   }
+
+  const getCards = () => {
+    fetch('http://localhost:4000/', {
+      method: "POST",
+      headers,
+      body: JSON.stringify({ query: "{ allCards { id sideA } }" }),
+    })
+      .then((response) => response.json())
+      .then(({ data }) => console.log(data));
+  }
+
+  useEffect(() => getCards())
 
   return (
     <Container>
